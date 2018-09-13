8b6292df1dd2adf021e2ce5df231cdfa887f4ae0
ukjolie2 <ukjolie2@gmail.com>
2018-09-12 22:55:07 -0600

    made camera controller for Thursday

---
+++ BloodMagic/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+    public float smoothSpeed = 0.125f;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        Vector3 finalPosition = Vector3.Lerp(transform.position, newPosition, smoothSpeed);
+        transform.position = finalPosition;
+    }
+}
