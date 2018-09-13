63562f997ac6dfea22b75660a8d1677601bea677
hinghing15 <huang.kathy.15@gmail.com>
2018-09-12 21:32:22 -0600

    Fix basic enemy ai

--- BloodMagic/Assets/Scripts/EnemyController.cs
+++ BloodMagic/Assets/Scripts/EnemyController.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour {
-    public GameObject target;
-    public float speed = 5f;
+public class EnemyController : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 3f;
 
-	void Update () {
+    void Update()
+    {
         transform.LookAt(target.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
         if (Vector3.Distance(transform.position, target.position) > 1f)
         {
             transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
