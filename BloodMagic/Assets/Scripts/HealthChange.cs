1ff187e0659cfcbf3d1099c8a4fb02af1673f560
Michael Caldwell <slowinfect@gmail.com>
2018-09-13 10:53:11 -0600

    Health UI attached to player health
    Health UI Re-saved over Prefab. Can be safely removed from Hierarchy.

--- BloodMagic/Assets/Scripts/HealthChange.cs
+++ BloodMagic/Assets/Scripts/HealthChange.cs
@@ -7,13 +7,16 @@ public class HealthChange : MonoBehaviour
     public Slider healthSlider;
     public Slider capSlider;
     //how much health we have?
-    public int CapCount = 25;
-    public int MaxHealth;
+    public int CapCount = 0; //Used for any effects that limit our Health Total
+    public int MaxHealth; //Assigned upon Start
+    
+    //Unused for now. Meant for slow transitions between current and lost health
     public int FillRate = 0;
     public int CapRate = 0;
+    //
 
 
-    public Image healthFill;  // assign in the editor the "Fill"
+    public Image healthFill;  // assign in the editor the "Fill" Used for Drawing the Health Bar and Color
     public Image capFill;
 
     //Different colors
@@ -25,17 +28,20 @@ public class HealthChange : MonoBehaviour
     }
     private void Start()
     {
+        MaxHealth = GameObject.Find("Player").GetComponent<PlayerController>().hp;
         healthSlider.wholeNumbers = true;
         healthSlider.minValue = 0f;
         healthSlider.maxValue = GameObject.Find("Player").GetComponent<PlayerController>().hp;
-        healthSlider.value = 0;
+        healthSlider.value = 0; //Variable Health Position
         capSlider.wholeNumbers = true;
         capSlider.minValue = 0f;
         capSlider.maxValue = GameObject.Find("Player").GetComponent<PlayerController>().hp;
-        capSlider.value = 0;
+        capSlider.value = 0; //Varaible Cap Position
     }
     private void Update()
     {
+
+        //Used for Health Slider updates for transitions between lost health. Will need more and altered code
         /*
         if (PracticalHealth <= counter)
         {
@@ -56,13 +62,16 @@ public class HealthChange : MonoBehaviour
             dcounter += CapRate;
         }
         */
-        UpdateHealthChange(counter, dcounter);
+
+        UpdateHealthChange(GameObject.Find("Player").GetComponent<PlayerController>().hp, 0); //Temporary Updater for immediate feedback
+      
     }
-    public void UpdateHealthChange(int health)
+
+    public void UpdateHealthChange(int health, int cap)
     {
         healthSlider.value = health;
-        capSlider.value = health;
+        capSlider.value = cap; //Right bar, currently unused
         capFill.color = DeadColor;
-        healthFill.color = Color.Lerp(MinHealthColor, MaxHealthColor, (float)health / MaxHealth);
+        healthFill.color = Color.Lerp(MinHealthColor, MaxHealthColor, (float)health / MaxHealth); //Gradient Color Transitions
     }
 }
