c2a658941be1f5a24e04732ae36f29f03adaf2cf
hinghing15 <huang.kathy.15@gmail.com>
2018-09-12 18:30:42 -0600

    Add basic enemy AI

---
+++ BloodMagic/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    public GameObject target;
+    public float speed = 5f;
+
+	void Update () {
+        transform.LookAt(target.position);
+        if (Vector3.Distance(transform.position, target.position) > 1f)
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+    }
+}
